2b7a98649ebac792d09f1b88a975d047c2483f20
jaeyoon <sc7258@gmail.com>
2019-07-02 17:11:18 +0900

    added external link

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,6 +28,8 @@
       </li>
     </ul>
     <h3>Essential Links</h3>
+    <h3>Essential Links</h3>
+    <h3>Essential Links</h3>
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
